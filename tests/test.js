e93befbadda365821b53d12a1517d3ef7c1ef1ac
xkusnir <maros.kusnir@gmail.com>
2021-03-18 15:40:20 +0100

    added negative test - non title

--- tests/test.js
+++ tests/test.js
@@ -6,7 +6,7 @@ const { title } = require('process');
 
 describe('end-to-end', () => {
 
-    var url="localhost:5000"
+    var url="localhost:5000/api"
 
     describe('create task', () => {
 
@@ -28,12 +28,13 @@ describe('end-to-end', () => {
             axios.post(url + "/todo", {newTask: newTask}, {headers: {"Authorization": `Bearer ${token}`}
             })
             .then(res => {
-                axios.get(url + "/todo/0")
+                axios.get(url + "/todo/userid")
                 var array = res.data
             })
             .then(res => {
                 array.sort((a,b) => a.id > b.id ? -1 : 1)
                 assert.strictEqual(array[0].title, newTask.title)
+                assert.strictEqual(array[0].type, newTask.type)
             })
         })
     })
@@ -51,25 +52,60 @@ describe('end-to-end', () => {
 
         it('new habit', () => {
 
-            var newTask = {
-                title: "Vysad strom",
-                type: 0
+            var newHabit = {
+                title: "Presad kvety",
+                type: 1,
+                perWeek: 1
             };
 
-            axios.post(url + "/todo", {
-                newTask: newTask
+            axios.post(url + "/habit", {newHabit: newHabit}, {headers: {"Authorization": `Bearer ${token}`}
             })
             .then(res => {
-                axios.get(url + "/todo/0")
+                axios.get(url + "/habit/userid")
                 var array = res.data
             })
             .then(res => {
                 array.sort((a,b) => a.id > b.id ? -1 : 1)
-                assert.strictEqual(array[0].title, newTask.title)
+                assert.strictEqual(array[0].title, newHabit.title)
+                assert.strictEqual(array[0].type, newHabit.type)
+                assert.strictEqual(array[0].perWeek, newHabit.perWeek)
+            })
+        })
+    })
+    
+
+    // negative test - no title 
+    describe('create task no title', () => {
+
+        before(() => {
+            axios.post(url + '/user/login', 
+            {email: "jozko.mrkvicka@gmail.com", password: ""})
+            .then((res) => {
+                token = res.data.token
+            })
+        })
+
+        it('new task', () => {
+
+            var newTask = {
+                title: null,
+                type: 0
+            };
+
+            axios.post(url + "/todo", {newTask: newTask}, {headers: {"Authorization": `Bearer ${token}`}
+            })
+            .then(res => {
+                axios.get(url + "/todo/userid")
+                var array = res.data
+            })
+            .then(res => {
+                array.sort((a,b) => a.id > b.id ? -1 : 1)
+                assert.strictEqual(array[0].title, newTask.title) // this fails, array[0] should not have title "null", null is not allowed!
+                assert.strictEqual(array[0].type, newTask.type)
             })
         })
     })
 
-
+    
 })
 
